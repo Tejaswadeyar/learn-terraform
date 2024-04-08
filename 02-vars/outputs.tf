dfa30360d7b4015d8b648b5dc1a04490591ce8a8
tejas wadeyar <Tejaswdeyar142@gmail.com>
2024-04-08 15:48:28 +0530

    Terraform learn

--- 02-vars/outputs.tf
+++ 02-vars/outputs.tf
@@ -1,3 +1,15 @@
 output "v1" {
   value = var.v1
 }
+
+output "First_Value_in_list" {
+  value = var.v2[0]
+}
+
+output "Second_value_in_list" {
+  value = var.v2[1]
+}
+
+output "value_in_map" {
+  value = var.v3["abc"]
+}
--- 02-vars/variables.tf
+++ 02-vars/variables.tf
@@ -1,3 +1,15 @@
 variable "v1"{
   default = "Hello world "
 }
+
+variable "v2" {
+  default = ["Hello world", 100, true ]
+}
+## A variable list can have different data types
+
+variable "v3" {
+  default = {
+    abc = 100
+    xyz = "Two Hundred"
+  }
+}
