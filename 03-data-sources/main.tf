7452d90ac5fb10732fedc875266683372f10dacc
tejas wadeyar <Tejaswdeyar142@gmail.com>
2024-04-09 21:01:08 +0530

    Terraform learn

--- 03-data-sources/main.tf
+++ 03-data-sources/main.tf
@@ -1,3 +1,5 @@
+#if ami_id changes then the code will noot run , therfore we declare the ami as below after declaring, even if thr ami changes u will get the ami according to the ami = owner and ami = name
+
 data "aws_ami" "example" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
---
+++ expense-v2/variables.tf
@@ -0,0 +1,17 @@
+#variable "ami" {
+#  default = "ami-0f3c7d07486cad139"
+#}
+
+
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+
+variable "vpc_security_group_ids" {
+  default = ["sg-0107f6cbfc7566933"]
+}
+variable "zone_id" {
+  default = "Z00984822ENLGYD99LZJY"
+}
