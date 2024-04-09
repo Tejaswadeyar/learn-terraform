19f548875373efa4c2d7db12f3a42d8948264ed2
tejas wadeyar <Tejaswdeyar142@gmail.com>
2024-04-09 20:09:51 +0530

    Terraform learn

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
-  ami                     = "ami-0f3c7d07486cad139"
-  instance_type           = "t3.micro"
-  vpc_security_group_ids  = ["sg-0107f6cbfc7566933"]
+  ami                     = var.ami
+  instance_type           = var.instance_type
+  vpc_security_group_ids  = var.vpc_security_group_ids
 
   tags = {
     Name = "frontend-dev"
@@ -9,7 +9,7 @@ resource "aws_instance" "frontend" {
 }
 
 resource "aws_route53_record" "frontend" {
-  zone_id = "Z00984822ENLGYD99LZJY"
+  zone_id = var.zone_id
   name    = "frontend-dev"
   type    = "A"
   ttl     = 30
@@ -19,9 +19,9 @@ resource "aws_route53_record" "frontend" {
 
 
 resource "aws_instance" "backend" {
-  ami                     = "ami-0f3c7d07486cad139"
-  instance_type           = "t3.micro"
-  vpc_security_group_ids  = ["sg-0107f6cbfc7566933"]
+  ami                     = var.ami
+  instance_type           = var.instance_type
+  vpc_security_group_ids  = var.vpc_security_group_ids
 
   tags = {
     Name = "backend-dev"
@@ -29,7 +29,7 @@ resource "aws_instance" "backend" {
 }
 
 resource "aws_route53_record" "backend" {
-  zone_id = "Z00984822ENLGYD99LZJY"
+  zone_id = var.zone_id
   name    = "backend-dev"
   type    = "A"
   ttl     = 30
@@ -39,9 +39,9 @@ resource "aws_route53_record" "backend" {
 
 
 resource "aws_instance" "mysql" {
-  ami                     = "ami-0f3c7d07486cad139"
-  instance_type           = "t3.micro"
-  vpc_security_group_ids  = ["sg-0107f6cbfc7566933"]
+  ami                     = var.ami
+  instance_type           = var.instance_type
+  vpc_security_group_ids  = var.vpc_security_group_ids
 
   tags = {
     Name = "mysql-dev"
@@ -49,7 +49,7 @@ resource "aws_instance" "mysql" {
 }
 
 resource "aws_route53_record" "mysql" {
-  zone_id = "Z00984822ENLGYD99LZJY"
+  zone_id = var.zone_id
   name    = "mysql-dev"
   type    = "A"
   ttl     = 30
