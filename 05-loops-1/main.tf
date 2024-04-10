6fb95bf93f3f5ef1ec4d110edd92797f1a709d3c
tejas wadeyar <Tejaswdeyar142@gmail.com>
2024-04-11 00:20:00 +0530

    Terraform learn

--- 05-loops-1/main.tf
+++ 05-loops-1/main.tf
@@ -10,8 +10,24 @@ output "test" {
 }
 
 
+#resource "aws_instance" "test" {
+#  count                   = 3
+#  ami                     = "ami-0f3c7d07486cad139"
+#  instance_type           = "t3.micro"
+#  vpc_security_group_ids  = ["sg-0107f6cbfc7566933"]
+#
+#  tags = {
+#    Name = "test-${count.index}"
+#  }
+#}
+
+variable "component" {
+  default = [ "frontend", "backend", "mysql"]
+}
+
+
 resource "aws_instance" "test" {
-  count                   = 3
+  count                   = length(var.component)
   ami                     = "ami-0f3c7d07486cad139"
   instance_type           = "t3.micro"
   vpc_security_group_ids  = ["sg-0107f6cbfc7566933"]
