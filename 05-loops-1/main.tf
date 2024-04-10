6c6fd4339bc1f010cafd99224e10b8b1f501df86
tejas wadeyar <Tejaswdeyar142@gmail.com>
2024-04-10 18:02:59 +0530

    Terraform learn

--- 05-loops-1/main.tf
+++ 05-loops-1/main.tf
@@ -8,3 +8,15 @@ resource "null_resource" "test" {
 output "test" {
   value = null_resource.test.*.id
 }
+
+
+resource "aws_instance" "test" {
+  count                   = 3
+  ami                     = "mi-0f3c7d07486cad139"
+  instance_type           = "t3.micro"
+  vpc_security_group_ids  = ["sg-0107f6cbfc7566933"]
+
+  tags = {
+    Name = "frontend-dev"
+  }
+}
--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -1,12 +1,3 @@
-resource "aws_instance" "frontend" {
-  ami                     = var.ami
-  instance_type           = var.instance_type
-  vpc_security_group_ids  = var.vpc_security_group_ids
-
-  tags = {
-    Name = "frontend-dev"
-  }
-}
 
 resource "aws_route53_record" "frontend" {
   zone_id = var.zone_id
