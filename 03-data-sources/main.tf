a15284511cb1125b79700f2a18d24b1467eb4ca2
tejas wadeyar <Tejaswdeyar142@gmail.com>
2024-05-04 22:50:04 +0530

    Terraform-expense = AutoScaling creation

--- 03-data-sources/main.tf
+++ 03-data-sources/main.tf
@@ -1,4 +1,4 @@
-#if ami_id changes then the code will noot run , therfore we declare the ami as below after declaring, even if thr ami changes u will get the ami according to the ami = owner and ami = name
+#if ami_id changes then the code will not run , therfore we declare the ami as below after declaring, even if thr ami changes u will get the ami according to the ami = owner and ami = name
 
 data "aws_ami" "example" {
   most_recent = true
